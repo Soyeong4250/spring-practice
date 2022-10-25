4ea09409f744ece886fdcc5db50da4ea9bafe1de
이소영 <th42500@naver.com>
2022-10-25 20:11:02 +0900

    [Refactor] Spring 적용을 위한 어노테이션 적용 및 코드 수정

--- src/main/java/com/sping/dao/UserDaoFactory.java
+++ src/main/java/com/sping/dao/UserDaoFactory.java
@@ -1,6 +1,12 @@
 package com.sping.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
+
+    @Bean
     public UserDao awsUserDao() {
         ConnectionMaker connectionMaker = new AWSConnectionMaker();
         return new UserDao(connectionMaker);
--- src/test/java/com/sping/dao/UserDaoTest.java
+++ src/test/java/com/sping/dao/UserDaoTest.java
@@ -4,15 +4,24 @@ import com.sping.domain.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+    @Autowired
+    ApplicationContext context;
     UserDao userDao;
 
     @BeforeEach
     void setUp() {
-        userDao = new UserDaoFactory().awsUserDao();
+        userDao = context.getBean("awsUserDao", UserDao.class);
         userDao.deleteAll();
     }
 
